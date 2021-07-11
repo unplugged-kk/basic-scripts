e56fa86b623b4764bbb7cbc51d274da630665989
unplugged-kk <kishore.behera2010@gmail.com>
2021-07-11 23:52:25 +0530

    Initial commit

--- sandboxauth.sh
+++ sandboxauth.sh
@@ -5,6 +5,16 @@ read PROJECT_ID
 
 export SA=tf-acg-gcp-sandbox
 
+echo " User id is $USER"
+echo " Project id is $PROJECT_ID"
+echo " Service Account id is $SA"
+
+echo " Enabling necessary APIS "
+
+gcloud services enable container.googleapis.com storage.googleapis.com anthos.googleapis.com anthosgke.googleapis.com cloud.googleapis.com cloudresourcemanager.googleapis.com containerregistry.googleapis.com file.googleapis.com ; gcloud services list --enable
+
+echo " Creating Service Account "
+
 gcloud iam service-accounts create $SA --display-name="$SA"
 
 gcloud projects add-iam-policy-binding $PROJECT_ID \
